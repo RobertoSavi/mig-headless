f230abeefbcd03b10b2a2dd813ccb43cfb519863
Roberto <roberto.savi2582@gmail.com>
2024-07-29 20:43:36 +0200

    Removed helper and old txt tests
    Removing need to copy Input folder in tools
    
    Update run_tests.yml
    
    Trying with msg_def.json inside docker volume
    
    Update run_tests.yml
    
    Update run_tests.yml
    
    Update run_tests.yml
    
    Update run_tests.yml
    
    Update run_tests.sh
    
    Update run_tests.yml
    
    Removed useless msg_def.json
    
    Cleaned final version
    
    Testing docker volumes mounting
    
    Update docker-compose.yml
    
    Update run_tests.yml
    
    Update run_tests.yml
    
    Final script and test file
    
    Removed input files from tools/i-mig-t
    
    Update run_tests.yml
    
    Update run_tests.yml
    
    Update run_tests.yml
    
    Update run_tests.yml
    
    Update run_tests.yml
    
    Update run_tests.yml
    
    Update build_and_run.sh
    
    Update run_tests.sh

--- testplans/spid-cie-oidc/implementations/spid-cie-oidc-django/build_and_run.sh
+++ testplans/spid-cie-oidc/implementations/spid-cie-oidc-django/build_and_run.sh
@@ -7,10 +7,6 @@ rm ./spid-cie-oidc-django/docker-compose.yml
 cp ./edited_files/docker-compose.yml ./spid-cie-oidc-django/
 rm ./spid-cie-oidc-django/Dockerfile
 cp ./edited_files/Dockerfile ./spid-cie-oidc-django/
-rm ./spid-cie-oidc-django/helper.Dockerfile
-cp ./edited_files/helper.Dockerfile ./spid-cie-oidc-django/
-cp ./run_tests.sh ./spid-cie-oidc-django/
-cp ./test1.json ./spid-cie-oidc-django/
 cd spid-cie-oidc-django
 bash docker-prepare.sh
 
